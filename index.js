5546e0b53bd8ff72e58076e36761ea16decff347
Willlasbury <asburyw229@gmail.com>
2023-04-26 14:56:22 -0700

    added index.js to project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const allRoutes = require('./controllers');
+app.use(allRoutes)
+
+app.listen(PORT,()=>{
+console.log('listening on port' + PORT)})
