d92e17fafa132629da014016091a80887f43c772
Willlasbury <asburyw229@gmail.com>
2023-04-26 17:25:53 -0700

    added route to notes html

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,10 @@ const path = require('path')
 
 
 router.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, ".."))
+    res.sendFile(path.join(__dirname, "../public"))
 })
+
+const notesRoute = require('./notes')
+router.use("/notes", notesRoute)
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const router = express.Router()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
@@ -9,6 +10,9 @@ app.use(express.json());
 
 const allRoutes = require('./controllers');
 app.use(allRoutes)
+
+
+app.use(router)
 
 app.listen(PORT,()=>{
 console.log('listening on port http://localhost:' + PORT)})
